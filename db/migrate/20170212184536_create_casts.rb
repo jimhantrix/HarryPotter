b458c3b4456e5b90618dbfda9cf338cc9ff6e9cc
jimhantrix <jimtekanyo@gmail.com>
2017-02-12 10:59:55 -0800

    first commit

---
+++ db/migrate/20170212184536_create_casts.rb
@@ -0,0 +1,12 @@
+class CreateCasts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :casts do |t|
+      t.string :character
+      t.string :location
+      t.string :qoute
+      t.string :book
+
+      t.timestamps
+    end
+  end
+end
